ce0c952d0ff45d3293e99bd0f767b89178b3376b
kleberaluizio <kleberaluizio@gmail.com>
2024-08-02 09:42:17 -0300

    Estudando o conceito de yield e volatile

---
+++ code/src/Volatile.java
@@ -0,0 +1,26 @@
+public class Volatile
+{
+	private static int number = 0;
+	private static boolean ready = false;
+
+	public static class MainRunnable implements Runnable {
+
+		@Override
+		public void run()
+		{
+			while (!ready){
+				Thread.yield();
+			}
+
+			System.out.println(number);
+		}
+	}
+	public static void main(String[] args)
+	{
+		Thread t0 = new Thread(new MainRunnable());
+		t0.start();
+		System.out.println(number);
+		number = 42;
+		ready = true;
+	}
+}
